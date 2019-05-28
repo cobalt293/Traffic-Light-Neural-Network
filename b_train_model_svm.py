97a1758a60a8c474972c4adb83651d03aa7c1e48
Andy OConnell <andy.oconnell@protonmail.com>
2019-05-28 18:47:30 -0400

    svm performance stats

--- b_train_model_svm.py
+++ b_train_model_svm.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn import svm
 import pickle
+import matplotlib.pyplot as plt
 
 FAILURE_MODEL = os.path.abspath('FailureModels/saved_model/svm.pickle')
 TRAINING_LOG = os.path.abspath('data/training_data_primary.csv')
@@ -45,4 +46,31 @@ model.fit(X_train, y_train)
 with open(FAILURE_MODEL, 'wb') as f:
     pickle.dump(model, f)
 
+
+
+# Calculate performance scores 
+y_test_pred = model.predict(X_test)
+y_test_scores = model.decision_function(X_test)
+
+from sklearn.metrics import precision_score, recall_score, f1_score, roc_curve
+model_precision_score = precision_score(y_test, y_test_pred)
+print("Precision Score: ", model_precision_score)
+
+model_recall_score = recall_score(y_test, y_test_pred)
+print("Recall Score: ", model_recall_score)
+
+model_f1_score = f1_score(y_test, y_test_pred)
+print("F1 Score: ", model_f1_score)
+
+def plot_roc_curve(fpr, tpr, label=None):
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0,1], [0,1], 'k--')
+    plt.axis([0,1,0,1])
+    plt.xlabel('False Pasitive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.show()
+
+fpr, tpr, thresholds = roc_curve(y_test, y_test_scores)
+
+plot_roc_curve(fpr, tpr)
 
